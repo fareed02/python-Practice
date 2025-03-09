ff471697571465078f9065f6c0f8e99a1962f89f
fareed02 <fareedevs@gmail.com>
2025-03-09 12:12:11 +0500

    adding my python practice file

---
+++ index.py
@@ -0,0 +1,42 @@
+# home = 'hgd'
+# print(home)
+
+
+# is_age = int (input("Enter your age"))
+# gender = input("enter your gender")
+# marital_status =  input("enter your status")
+
+
+# if (gender == "female") and (marital_status == "single"): 
+#      print("you are eligible to work in urban areas")
+
+# elif (gender == "female") and (marital_status == "married"):
+#      print("you are not eligible to work in urban areas")
+
+# elif (gender == "male") and (is_age > 20 and is_age <40):
+#      print("you are eligible to work in anywhrere")
+
+# elif (gender == "male") and (is_age > 40 and is_age < 70):
+#      print("you are eligible to work in urban areas areas")
+
+# elif (gender == "male") and (is_age >= 20):
+#      print("sorry you are not eligible to worl anywhere")
+# else :
+#      print("syntax error")
+
+
+Countires = ["pakistan", "india", "iran", "itly", "japan" , "america"]
+# # print(Countires[-5:-2])
+# Countires.remove('india')
+# print(Countires)
+
+country = Countires.pop(1)
+print(country)
+# Countires.append("England")
+# Countires.insert(5, "korea")
+# Countires[5] = "america"
+
+# Countires[1:4]
+
+# print(Countires[0])
+
